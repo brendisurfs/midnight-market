debc2a253c34363f21824e7de96e3aa0b154c9d7
Brendan Prednis <brendan.prednis@protonmail.com>
2021-12-11 11:07:06 -0800

    init commit.
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+
+const fs = require("fs");
+
+const privateKey = fs.readFileSync(".secret").toString();
+
+// work with infura.
+const projectID = "02c51f59c71643019b77aa0e84a9fb23";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    networks: {
+        hardhat: {
+            chainId: 1337,
+        },
+        mumbai: {
+            url: `https://polygon-mumbai.infura.io/v3/${projectID}`,
+            accounts: [privateKey],
+        },
+        mainnet: {
+            url: `https://polygon-mainnet.infura.io/v3/${projectID}`,
+            accounts: [privateKey],
+        },
+    },
+    solidity: "0.8.4",
+};
